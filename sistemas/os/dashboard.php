310a3b78db9c4eb8cb1e77278a85a01dc6ba6f46
Marcello F. Vieira <marcello.strategy@gmail.com>
2024-10-18 19:49:31 -0500

    template with graphs

--- sistemas/os/dashboard.php
+++ sistemas/os/dashboard.php
@@ -1,3 +1,61 @@
+<?php
+
+$dataPoints = array(
+    array("x"=> 10, "y"=> 41),
+    array("x"=> 20, "y"=> 35, "indexLabel"=> "Lowest"),
+    array("x"=> 30, "y"=> 50),
+    array("x"=> 40, "y"=> 45),
+    array("x"=> 50, "y"=> 52),
+    array("x"=> 60, "y"=> 68),
+    array("x"=> 70, "y"=> 38),
+    array("x"=> 80, "y"=> 71, "indexLabel"=> "Highest"),
+    array("x"=> 90, "y"=> 52),
+    array("x"=> 100, "y"=> 60),
+    array("x"=> 110, "y"=> 36),
+    array("x"=> 120, "y"=> 49),
+    array("x"=> 130, "y"=> 41)
+);
+
+$dataPoints2 = array(
+    array("label"=> "Food + Drinks", "y"=> 590),
+    array("label"=> "Activities and Entertainments", "y"=> 261),
+    array("label"=> "Health and Fitness", "y"=> 158),
+    array("label"=> "Shopping & Misc", "y"=> 72),
+    array("label"=> "Transportation", "y"=> 191),
+    array("label"=> "Rent", "y"=> 573),
+    array("label"=> "Travel Insurance", "y"=> 126)
+);
+
+$dataPoints3 = array(
+    array("label"=> 1992, "y"=>105),
+    array("label"=> 1993, "y"=>130),
+    array("label"=> 1994, "y"=>158),
+    array("label"=> 1995, "y"=>192),
+    array("label"=> 1996, "y"=>309),
+    array("label"=> 1997, "y"=>422),
+    array("label"=> 1998, "y"=>566),
+    array("label"=> 1999, "y"=>807),
+    array("label"=> 2000, "y"=>1250),
+    array("label"=> 2001, "y"=>1615),
+    array("label"=> 2002, "y"=>2069),
+    array("label"=> 2003, "y"=>2635),
+    array("label"=> 2004, "y"=>3723),
+    array("label"=> 2005, "y"=>5112),
+    array("label"=> 2006, "y"=>6660),
+    array("label"=> 2007, "y"=>9183),
+    array("label"=> 2008, "y"=>15844),
+    array("label"=> 2009, "y"=>23185),
+    array("label"=> 2010, "y"=>40336),
+    array("label"=> 2011, "y"=>70469),
+    array("label"=> 2012, "y"=>100504),
+    array("label"=> 2013, "y"=>138856),
+    array("label"=> 2014, "y"=>178391),
+    array("label"=> 2015, "y"=>229300),
+    array("label"=> 2016, "y"=>302300),
+    array("label"=> 2017, "y"=>368000)
+);
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="">
 
@@ -17,6 +75,120 @@
             overflow-x: auto;
         }
     </style>
+    <script>
+        window.onload = function () {
+
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                exportEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                    text: "Energia"
+                },
+                axisY:{
+                    includeZero: true
+                },
+                data: [{
+                    type: "column", //change type to bar, line, area, pie, etc
+                    //indexLabel: "{y}", //Shows y value on all Data Points
+                    indexLabelFontColor: "#5A5757",
+                    indexLabelPlacement: "outside",
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render();
+
+            var chart2 = new CanvasJS.Chart("chartContainer2", {
+                animationEnabled: true,
+                exportEnabled: true,
+                title:{
+                    text: "Agua"
+                },
+                subtitles: [{
+                    text: "lorem sumpin depuil"
+                }],
+                data: [{
+                    type: "pie",
+                    showInLegend: "true",
+                    legendText: "{label}",
+                    indexLabelFontSize: 16,
+                    indexLabel: "{label} - #percent%",
+                    yValueFormatString: "฿#,##0",
+                    dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart2.render();
+
+            var chart3 = new CanvasJS.Chart("chartContainer3", {
+                animationEnabled: true,
+                theme: "light2",
+                title:{
+                    text: "Preventiva "
+                },
+                axisY:{
+                    title: "Energy (in megawatt)",
+                    logarithmic: true,
+                    titleFontColor: "#6D78AD",
+                    gridColor: "#6D78AD",
+                    includeZero: true,
+                    labelFormatter: addSymbols
+                },
+                axisY2:{
+                    title: "Energy (in megawatt)",
+                    titleFontColor: "#51CDA0",
+                    tickLength: 0,
+                    labelFormatter: addSymbols
+                },
+                legend: {
+                    cursor: "pointer",
+                    verticalAlign: "top",
+                    fontSize: 16,
+                    itemclick: toggleDataSeries
+                },
+                data: [{
+                    type: "line",
+                    markerSize: 0,
+                    showInLegend: true,
+                    name: "Log Scale",
+                    yValueFormatString: "#,##0 MW",
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                },
+                    {
+                        type: "line",
+                        markerSize: 0,
+                        axisYType: "secondary",
+                        showInLegend: true,
+                        name: "Linear Scale",
+                        yValueFormatString: "#,##0 MW",
+                        dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+                    }]
+            });
+            chart3.render();
+
+            function addSymbols(e){
+                var suffixes = ["", "K", "M", "B"];
+
+                var order = Math.max(Math.floor(Math.log(Math.abs(e.value)) / Math.log(1000)), 0);
+                if(order > suffixes.length - 1)
+                    order = suffixes.length - 1;
+
+                var suffix = suffixes[order];
+                return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+            }
+
+            function toggleDataSeries(e){
+                if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                }
+                else{
+                    e.dataSeries.visible = true;
+                }
+                chart.render();
+            }
+        }
+
+
+    </script>
 </head>
 
 <body class="text-left">
@@ -42,59 +214,99 @@
             <div class="separator-breadcrumb border-top"></div>
 
 
+<!--            <div class="row">-->
+<!--                <!-- ICON BG -->
+<!--                <div class="col-lg-3 col-md-6 col-sm-6">-->
+<!--                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">-->
+<!--                        <div class="card-body text-center">-->
+<!--                            <i class="i-File-Clipboard-File--Text"></i>-->
+<!--                            <div class="content">-->
+<!--                                <p class="text-muted mt-2 mb-0">ABERTA</p>-->
+<!--                                <p class="text-primary text-24 line-height-1 mb-2">25</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-lg-3 col-md-6 col-sm-6">-->
+<!--                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">-->
+<!--                        <div class="card-body text-center">-->
+<!--                            <i class="i-Financial"></i>-->
+<!--                            <div class="content">-->
+<!--                                <p class="text-muted mt-2 mb-0">FECHADA</p>-->
+<!--                                <p class="text-primary text-24 line-height-1 mb-2">21</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-lg-3 col-md-6 col-sm-6">-->
+<!--                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">-->
+<!--                        <div class="card-body text-center">-->
+<!--                            <i class="i-Checkout-Basket"></i>-->
+<!--                            <div class="content">-->
+<!--                                <p class="text-muted mt-2 mb-0">CANCELADAS</p>-->
+<!--                                <p class="text-primary text-24 line-height-1 mb-2">80</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-lg-3 col-md-6 col-sm-6">-->
+<!--                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">-->
+<!--                        <div class="card-body text-center">-->
+<!--                            <i class="i-Money-2"></i>-->
+<!--                            <div class="content">-->
+<!--                                <p class="text-muted mt-2 mb-0">TOTAL OS</p>-->
+<!--                                <p class="text-primary text-24 line-height-1 mb-2">220</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+
             <div class="row">
-                <!-- ICON BG -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">
-                        <div class="card-body text-center">
-                            <i class="i-File-Clipboard-File--Text"></i>
-                            <div class="content">
-                                <p class="text-muted mt-2 mb-0">ABERTA</p>
-                                <p class="text-primary text-24 line-height-1 mb-2">25</p>
+
+                <!-- Graficos -->
+                <div class="col-lg-4 col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center">
+                            <h3 class="w-50 float-left card-title m-0">Grafico de Energia</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div id="chartContainer" style="height: 270px; width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center">
+                            <h3 class="w-50 float-left card-title m-0">Grafico de Agua</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div id="chartContainer2" style="height: 270px; width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center">
+                            <h3 class="w-50 float-left card-title m-0">Grafico Preventiva</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div id="chartContainer3" style="height: 270px; width: 100%;"></div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">
-                        <div class="card-body text-center">
-                            <i class="i-Financial"></i>
-                            <div class="content">
-                                <p class="text-muted mt-2 mb-0">FECHADA</p>
-                                <p class="text-primary text-24 line-height-1 mb-2">21</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">
-                        <div class="card-body text-center">
-                            <i class="i-Checkout-Basket"></i>
-                            <div class="content">
-                                <p class="text-muted mt-2 mb-0">CANCELADAS</p>
-                                <p class="text-primary text-24 line-height-1 mb-2">80</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-icon-bg card-icon-bg-primary o-hidden mb-4">
-                        <div class="card-body text-center">
-                            <i class="i-Money-2"></i>
-                            <div class="content">
-                                <p class="text-muted mt-2 mb-0">TOTAL OS</p>
-                                <p class="text-primary text-24 line-height-1 mb-2">220</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="row">
+                <!-- End of Graficos -->
 
                 <!-- New Users -->
                 <div class="col-md-8">
@@ -482,6 +694,9 @@
     <script src="assets/js/vendor/apexcharts.min.js"></script>
     <script src="assets/js/vendor/echarts.min.js"></script>
     <script src="assets/js/es5/echart.options.min.js"></script>
+
+    <!-- Different type of Chart Canvas -->
+    <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 </body>
 
 </html>
